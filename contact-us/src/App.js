f6f1bd9cc23c73260c010d918c28151528fee8fc
ooluchi <ooluchiteeto@gmail.com>
2023-05-07 12:39:21 +0100

    first commit

---
+++ contact-us/src/App.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 Your Company Name
+ * All rights reserved.
+ */
+
+import './App.css';
+
+function App() {
+  return (
+    null
+  )
+}
+
+export default App;
---
+++ contact-us/src/index.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Your Company Name
+ * All rights reserved.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
